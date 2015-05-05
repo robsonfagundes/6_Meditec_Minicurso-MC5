5a5be460e6f1a3c0598889ded2007599c59c4ed3
robsonfagundes <robsonfagundes@gmail.com>
2015-05-05 14:04:02 -0300

    6 Meditec - Minicurso-MC5

--- src/br/net/meditec/app/AppMeditecActivity.java
+++ src/br/net/meditec/app/AppMeditecActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class AppMeditecActivity extends DroidGap {
     /** Called when the activity is first created. */
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
